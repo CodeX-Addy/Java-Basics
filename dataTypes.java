fba8b15348154f7b319385ec38fa1e9b1637fb15
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-10-05 17:47:50 +0530

    Create dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,26 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Sum{
+    public static void main(String []args){
+        //INTEGERS
+        int a = 5;
+        short b = 2;
+        long c = 8;
+        byte d = 6;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print("\n");
+        System.out.println(c);
+        System.out.print(d);
+        
+        System.out.print("\n");
+        //Float
+        float f = 1.6f;
+        double e = 1.6;
+        System.out.println(f);
+        System.out.println(e);
+    }
+}
+
