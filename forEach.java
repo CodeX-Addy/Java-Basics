3b43952c78cde1b2b8c34290489c1e28f6635d3d
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-10-07 11:45:10 +0530

    Enhanced for loop in java

--- forEach.java
+++ forEach.java
@@ -1,19 +1,16 @@
 //Concept of method overloading
-class Calculator{
-  public int add(int a, int b){
-    return a+b;
-  }
+// class Calculator{
+//   public int add(int a, int b){
+//     return a+b;
+//   }
 
-  public int add(int a, int b, int c){
-    return a+b+c;
-  }
-}
+//   public int add(int a, int b, int c){
+//     return a+b+c;
+//   }
+// }
 
 class Main{
   public static void main(String[] args){
-    // Calculator obj = new Calculator();
-    // System.out.println(obj.add(2,3));
-    // System.out.println(obj.add(2,3,4));
     int arr[] = new int[5];
     arr[0] = 1;arr[1] = 2;arr[2] = 3;arr[3] = 4;
     arr[4] = 5;
