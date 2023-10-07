b258218803f80400de10bf52af45b4d168d977e1
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-10-07 12:42:59 +0530

    For immutable strings

---
+++ stringBuffer.java
@@ -0,0 +1,17 @@
+class Main{
+  public static void main(String []args){
+    String st = new String("Aditya");
+    // System.out.println("Hello " + st);
+    // System.out.println(st.charAt(2));
+    // System.out.println(st.concat(" Tomar "));
+
+    System.out.println(st);
+
+    StringBuffer sb = new StringBuffer("Aditya");
+    System.out.println(sb.capacity());
+    System.out.println(sb.length());
+    
+    
+    
+  }
+}
